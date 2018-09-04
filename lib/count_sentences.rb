e2f4a0d78d2fcc6ee33540b32ca4ac6e06dd7d4c
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-09-04 18:23:16 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/["."|"!"+|"?"+]/).compact.reject{|h| h == " " | h == nil}.count
+    self.split(/["."|"!"+|"?"+]/).compact.reject{|h| h == " "}.count
   end
 end
 
